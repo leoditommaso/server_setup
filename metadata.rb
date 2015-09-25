c85ce73fa192bb3b0c7087dede989a151e6c6daa
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-09-25 20:24:14 -0300

    Added basic files.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'server_setup'
+maintainer       'Leandro Di Tommaso @ Mikroways'
+maintainer_email 'leandro.ditommaso@mikroways.net'
+license          'MIT'
+description      'Installs/Configures server_setup'
+long_description 'Installs/Configures server_setup'
+version          '0.1.0'
+
+depends 'apt'
